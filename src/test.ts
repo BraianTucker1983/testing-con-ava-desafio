977691b4fb8db87a9e7bd21e9823d4e2fc2a552e
unknown <braiantucker@gmail.com>
2024-09-12 06:45:08 -0300

    Test modificados

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,5 @@
 import test from "ava";
-import { Product } from "./index";
+import { Product, User } from "./index";
 
 test("Testeo la clase Product", (t) => {
   // test Product class
@@ -7,4 +7,17 @@ test("Testeo la clase Product", (t) => {
   t.is(product.name, "Product 1");
   t.is(product.price, 10);
 });
-test("Testeo la clase User y sus métodos", (t) => {});
+
+test("Testeo la clase User y sus métodos", (t) => {
+  const matilda = new User("Matilda");
+  const product = new Product("Product 1", 10);
+
+  t.is(matilda.name, "Matilda");
+
+  // Agregar el producto a la lista de productos del usuario
+  matilda.addProduct(product);
+
+  // Verificar que el producto fue agregado correctamente
+  t.is(matilda.products[0].name, "Product 1");
+  t.is(matilda.products[0].price, 10);
+});
